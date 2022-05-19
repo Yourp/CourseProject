12ed360ceabecfc528bddddaeebd649e2ddb545e
Oleksandr Kovalenko <bebekalka89@gmail.com>
2022-05-19 10:39:04 +0300

    Create blank project

---
+++ Source/CourseProject/CourseProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CourseProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COURSEPROJECT_API ACourseProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
